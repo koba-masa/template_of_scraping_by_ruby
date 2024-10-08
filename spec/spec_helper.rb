08154c01b41dff2eb0611c3e3683364a778cf6fb
koba-masa <kobayashi_mshr@koba-masa.com>
2024-10-08 15:29:08 +0900

    chore: VCRの設定を追加する

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -102,3 +102,9 @@ RSpec.configure do |config|
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
